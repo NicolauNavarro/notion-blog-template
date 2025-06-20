aae01a4311c257df61b1a021d015693625655835
Nicolau <149939145+NicolauNavarro@users.noreply.github.com>
2025-06-21 00:29:56 +0200

    query api call and showcase on client

--- src/actions/posts.ts
+++ src/actions/posts.ts
@@ -1,12 +1,16 @@
 "use server";
-
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { Client } from "@notionhq/client";
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 const databaseId = process.env.NOTION_DATABASE_ID || "";
 
-const isoNow = new Date().toISOString(); // always UTC
+const isoNow = new Date().toISOString();
+
+function isFullPageObject(obj: unknown): obj is PageObjectResponse {
+  return typeof obj === "object" && obj !== null && "properties" in obj;
+}
 
 export async function queryPosts() {
   const response = await notion.databases.query({
@@ -30,22 +34,91 @@ export async function queryPosts() {
     sorts: [
       {
         property: "Date",
-        direction: "ascending",
+        direction: "descending",
       },
     ],
   });
 
-  const posts = response.results.map((post) => ({
-    title: post.properties.Title.title[0]?.plain_text || "",
-    tags: post.properties.Tags.multi_select.map((tag) => tag.name),
-    date: post.properties.Date.date?.start || "",
-    slug: post.properties.Slug.rich_text[0]?.plain_text || "",
-    excerpt: post.properties.Excerpt.rich_text[0]?.plain_text || "",
-    published: post.properties.Published.checkbox,
-    coverImage:
-      post.properties["Cover Image"].files[0]?.file?.url ||
-      post.properties["Cover Image"].files[0]?.external?.url,
-  }));
+  const posts = response.results.filter(isFullPageObject).map((post) => {
+    const titleProperty = post.properties.Title;
+    const tagsProperty = post.properties.Tags;
+    const dateProperty = post.properties.Date;
+    const slugProperty = post.properties.Slug;
+    const excerptProperty = post.properties.Excerpt;
+    const publishedProperty = post.properties.Published;
+    const coverImageProperty = post.properties["Cover Image"];
+
+    const title =
+      titleProperty.type === "title"
+        ? titleProperty.title[0]?.plain_text || ""
+        : "";
+
+    const tags =
+      tagsProperty.type === "multi_select"
+        ? tagsProperty.multi_select.map((tag) => tag.name)
+        : [];
+
+    const date =
+      dateProperty.type === "date"
+        ? formatDate(dateProperty.date?.start || "")
+        : "";
+
+    const slug =
+      slugProperty.type === "rich_text"
+        ? slugProperty.rich_text[0]?.plain_text || ""
+        : "";
+
+    const excerpt =
+      excerptProperty.type === "rich_text"
+        ? excerptProperty.rich_text[0]?.plain_text || ""
+        : "";
+
+    const published =
+      publishedProperty.type === "checkbox"
+        ? publishedProperty.checkbox
+        : false;
+
+    const coverImage =
+      coverImageProperty.type === "files" && coverImageProperty.files.length > 0
+        ? coverImageProperty.files[0].type === "file"
+          ? coverImageProperty.files[0].file.url
+          : coverImageProperty.files[0].type === "external"
+          ? coverImageProperty.files[0].external.url
+          : ""
+        : "";
+
+    return {
+      title,
+      tags,
+      date,
+      slug,
+      excerpt,
+      published,
+      coverImage,
+    };
+  });
 
   return posts;
+}
+
+function formatDate(date: string) {
+  const newDate = new Date(date);
+  const day = newDate.getDate();
+  const year = newDate.getFullYear();
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = months[newDate.getMonth()];
+  return `${day} ${month} ${year}`;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { queryPosts } from "@/actions/posts";
 import Link from "next/link";
 
 export default async function Home() {
-  const response = await queryPosts();
-  console.log(response);
+  const posts = await queryPosts();
+  console.log(posts);
   return (
     <>
       <main className="overflow-hidden flex flex-col justify-center w-1/3 border-b-2 border-gray-200 pb-8">
@@ -19,90 +19,19 @@ export default async function Home() {
         </p>
       </main>
 
-      <Link
-        href="/"
-        className=" overflow-hidden flex flex-col justify-center gap-4 w-1/3 rounded-xl p-4 hover:bg-gray-100 transition-colors box-content relative"
-      >
-        <p className="text-sm text-gray-600 absolute right-4 top-4 h-4 flex items-center">
-          5 Jun 2025
-        </p>
-        <h2 className="text-2xl font-medium">The process</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          nesciunt consectetur aspernatur natus sunt magnam eos, eum blanditiis
-          maxime dolor ipsa voluptatem quia explicabo architecto error, esse
-          cupiditate?
-        </p>
-      </Link>
-
-      <Link
-        href="/"
-        className=" overflow-hidden flex flex-col justify-center gap-4 w-1/3 rounded-xl p-4 hover:bg-gray-100 transition-colors box-content relative"
-      >
-        <p className="text-sm text-gray-600 absolute right-4 top-4 h-4 flex items-center">
-          30 Apr 2025
-        </p>
-        <h2 className="text-2xl font-medium">Why AI is taking over all jobs</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          nesciunt consectetur aspernatur natus sunt magnam eos, eum blanditiis
-          maxime dolor ipsa voluptatem quia explicabo architecto error, esse
-          cupiditate?
-        </p>
-      </Link>
-
-      <Link
-        href="/"
-        className=" overflow-hidden flex flex-col justify-center gap-4 w-1/3 rounded-xl p-4 hover:bg-gray-100 transition-colors box-content relative"
-      >
-        <p className="text-sm text-gray-600 absolute right-4 top-4 h-4 flex items-center">
-          19 Apr 2025
-        </p>
-        <h2 className="text-2xl font-medium">Discipline is key</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          nesciunt consectetur aspernatur natus sunt magnam eos, eum blanditiis
-          maxime dolor ipsa voluptatem quia explicabo architecto error, esse
-          cupiditate?
-        </p>
-      </Link>
-
-      <Link
-        href="/"
-        className=" overflow-hidden flex flex-col justify-center gap-4 w-1/3 rounded-xl p-4 hover:bg-gray-100 transition-colors box-content relative"
-      >
-        <p className="text-sm text-gray-600 absolute right-4 top-4 h-4 flex items-center">
-          7 Feb 2025
-        </p>
-        <h2 className="text-2xl font-medium pr-28">
-          The untold story about vaccines and covid 19 reatons for communism
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          nesciunt consectetur aspernatur natus sunt magnam eos, eum blanditiis
-          maxime dolor ipsa voluptatem quia explicabo architecto error, esse
-          cupiditate?
-        </p>
-      </Link>
-
-      <Link
-        href="/"
-        className=" overflow-hidden flex flex-col justify-center gap-4 w-1/3 rounded-xl p-4 hover:bg-gray-100 transition-colors box-content relative"
-      >
-        <p className="text-sm text-gray-600 absolute right-4 top-4 h-4 flex items-center">
-          28 Jan 2025
-        </p>
-        <h2 className="text-2xl font-medium pr-28">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Animiadipisicing.
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          nesciunt consectetur aspernatur natus sunt magnam eos, eum blanditiis
-          maxime dolor ipsa voluptatem quia explicabo architecto error, esse
-          cupiditate?
-        </p>
-      </Link>
+      {posts.map((post) => (
+        <Link
+          key={post.slug}
+          href={`/${post.slug}`}
+          className=" overflow-hidden flex flex-col justify-center gap-4 w-1/3 rounded-xl p-4 hover:bg-gray-100 transition-colors box-content relative"
+        >
+          <p className="text-sm text-gray-600 absolute right-4 top-4 h-4 flex items-center">
+            {post.date}
+          </p>
+          <h2 className="text-2xl font-medium pr-28">{post.title}</h2>
+          <p>{post.excerpt}</p>
+        </Link>
+      ))}
     </>
   );
 }
